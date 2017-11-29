a18a36c62dd7035f74399d805c6c2deff5f4223c
Idan Tabachnik <idantb101@gmail.com>
2017-11-29 12:49:55 +0200

    changin the host name for heroku Hosting the app in heroku #15

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const router = require('./router.js');
 
-const host = process.env.HOST || 'localhost';
+const host = '0.0.0.0' || process.env.HOST || 'localhost';
 const port = process.env.PORT || 3000;
 
 http.createServer(router).listen(port, host, () => {
