abe3b41fa29ba91aa392aa49a6900350f5523f52
ionush <neilasjoseph@gmail.com>
2017-11-29 13:50:38 +0200

    added maps listeners to produce xy coordinate and edited index.html accordindly. Also added fetch.js
    this refers to issues #27 #21 #29

---
+++ js/fetch.js
@@ -0,0 +1,18 @@
+function fetch(url,callBack){
+  var xhr= new XMLHttpRequest();
+  xhr.onreadystatechange = function(){
+    console.log('readyState' + xhr.readyState);
+    console.log('status' + xhr.status);
+    if(xhr.readyState == 4 && xhr.status != 200){
+      callBack(true);
+    }
+
+    if(xhr.readyState == 4 && xhr.status == 200){
+
+      console.log('responseText' + xhr.responseText)
+      callBack(null, JSON.parse(xhr.responseText));
+    }
+  };
+  xhr.open("GET",url);
+  xhr.send();
+}
--- src/model.js
+++ src/model.js
@@ -1,4 +1,5 @@
 const data = require('./db/locations.json');
+const http = require('http');
 
 const autocomplete = (searchterm) => {
   const newArr = [];
@@ -15,5 +16,13 @@ const autocomplete = (searchterm) => {
 }
 
 
+const instaLoc = (searchterm) =>{
+  //https://api.instagram.com/v1/locations/{location-id}/media/recent?access_token=ACCESS-TOKEN
+}
+
+const instaXy = (searchterm) =>{
+  //https://api.instagram.com/v1/locations/search?lat=48.858844&lng=2.294351&access_token=ACCESS-TOKEN
+  const apiLink = `https://api.instagram.com/v1/locations/search?${searchterm}&access_token=ACCESS-TOKEN`
+}
 
 module.exports= {autocomplete}
